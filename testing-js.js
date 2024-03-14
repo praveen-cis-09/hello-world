5175975ae68615e0991861ffde5b2d393e548262
praveen s <praveen.s@cisinlabs.com>
2024-03-14 18:39:44 +0530

    Added a testing file.

---
+++ testing-js.js
@@ -0,0 +1,12 @@
+let countDown = 10;
+
+
+while (countDown >= 0) {
+    console.log('countDown', countDown)
+    countDown--;
+}
+
+for(;countDown;){
+    console.log('countDown',countDown);
+    countDown--
+}
